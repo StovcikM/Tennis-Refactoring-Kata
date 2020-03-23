5d353feb4f651c9aacf62dcfe4331aedf218c253
Matus Stovcik <Brigdnk@UVT-BRIGADNI-NB.staff.ucn.muni.cz>
2020-03-23 14:03:30 +0100

    TennisGame1

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -15,66 +15,88 @@ namespace Tennis
 
         public void WonPoint(string playerName)
         {
-            if (playerName == "player1")
-                m_score1 += 1;
-            else
-                m_score2 += 1;
+            _ = (playerName == "player1") ? m_score1 += 1 : m_score2 += 1;
         }
-
+ 
         public string GetScore()
         {
-            string score = "";
-            var tempScore = 0;
+            string score;
+
             if (m_score1 == m_score2)
             {
-                switch (m_score1)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
-                }
+                score = EqualPoints();
             }
             else if (m_score1 >= 4 || m_score2 >= 4)
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                score = FourAndMorePoints();
             }
             else
             {
-                for (var i = 1; i < 3; i++)
+                score = LessThenFourPoints();
+            }
+            return score;
+        }
+
+        private string EqualPoints()
+        {
+            string score;
+            switch (m_score1)
+            {
+                case 0:
+                    score = "Love-All";
+                    break;
+                case 1:
+                    score = "Fifteen-All";
+                    break;
+                case 2:
+                    score = "Thirty-All";
+                    break;
+                default:
+                    score = "Deuce";
+                    break;
+            }
+
+            return score;
+        }
+
+        private string LessThenFourPoints()
+        {
+            string score = "";
+            int tempScore;
+            for (var i = 1; i < 3; i++)
+            {
+                if (i == 1) tempScore = m_score1;
+                else { score += "-"; tempScore = m_score2; }
+                switch (tempScore)
                 {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
+                    case 0:
+                        score += "Love";
+                        break;
+                    case 1:
+                        score += "Fifteen";
+                        break;
+                    case 2:
+                        score += "Thirty";
+                        break;
+                    case 3:
+                        score += "Forty";
+                        break;
                 }
             }
+
+            return score;
+        }
+
+        private string FourAndMorePoints()
+        {
+            string score = "Advantage ";
+            var minusResult = m_score1 - m_score2;
+            
+            if (minusResult == 1) score += "player1";
+            else if (minusResult == -1) score += "player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
+
             return score;
         }
     }
